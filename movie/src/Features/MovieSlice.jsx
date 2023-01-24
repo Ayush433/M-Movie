f10aa7628dccd8aa307be98d8376f9b6f8dbe46f
Ayush Adhikari <adhikariayush19@gmail.comgit config --global user.nameAyush433>
2023-01-24 17:46:20 +0545

    Redux Toolkit setup

---
+++ movie/src/Features/MovieSlice.jsx
@@ -0,0 +1,19 @@
+import { createAction } from "@reduxjs/toolkit";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const MovieSlice = createApi({
+  reducerPath: "movieApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://api.themoviedb.org/3",
+  }),
+  endpoints: (builder) => ({
+    getMovieByCategory: builder.query({
+      query: (query) => ({
+        url: `movie/${query}`,
+        params: {
+          api_key: "2a0f926961d00c667e191a21c14461f8",
+        },
+      }),
+    }),
+  }),
+});
